63e805f0187c8f877814da8b5327165e22744ea4
LeoSallu <L.sallustio99@gmail.com>
2023-02-23 15:27:56 +0100

    Fizzbuzz

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 // PROGRAMMA PER IL CONTEGGO
 for (let i = 1; i <= 100; i++){
+    if(i % 15==0){
+        console.log('Fizzbuzz');
+    }else if(i % 5==0){
+        console.log('Buzz');
+    }    
+    else if(i % 3==0){
+        console.log('Fizz')
+    }
     console.log(i);
 }
