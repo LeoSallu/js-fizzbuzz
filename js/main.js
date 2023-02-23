2816a30b04cd411884658f008b60c854a7ac5ebc
LeoSallu <L.sallustio99@gmail.com>
2023-02-23 15:41:02 +0100

    fix FIzzbuzz

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
-// PROGRAMMA PER IL CONTEGGO
+'use strict'
+// PROGRAMMA PER IL CONTEGGIO
 for (let i = 1; i <= 100; i++){
     if(i % 15==0){
-        console.log('Fizzbuzz');
+        console.log("Fizzbuzz");
     }else if(i % 5==0){
-        console.log('Buzz');
+        console.log("Buzz");
     }    
     else if(i % 3==0){
-        console.log('Fizz')
+        console.log("Fizz")
     }
-    console.log(i);
+    else{
+        console.log(i);
+    }
 }
